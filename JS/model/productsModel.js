4b89a36c71f07fe168e4dec4113ba2026c197533
Zapiony <sazapatac4@gmail.com>
2025-11-20 08:28:59 -0500

    Restructuración del proyecto (#12)
    - Se restructuró todos los archivos del proyecto
    - Se divide solo en 4 carpetas principales (html, css, js, img)
    
    Co-authored-by: emilypam <emilyp131204@gmail.com>

---
+++ JS/model/productsModel.js
@@ -0,0 +1,33 @@
+const productos = [
+    {id: 1, nombre: "Airpods última generación",  detalle:"Con opción de cancelación de ruido", puntuación: 4.5, opiniones: 200,precio: 50},
+    {id: 2, nombre: "Iphone X",  detalle:"Con batería más resistente", puntuación: 3.5, opiniones: 800,precio: 1200},
+    {id: 3, nombre: "MacBook Pro",  detalle:"Potente y elegante", puntuación: 2.5, opiniones: 1000,precio: 5000}
+]
+
+class Products {
+
+    constructor() {
+        this.Products = productos;
+        this.cart = [];
+    }
+
+    getProducts() {
+        return this.Products;
+    }
+
+    getProductById(id) {
+        return find(p => 
+            p.id == id
+        )
+    }
+
+    getProductoDest() {
+        return find(p => 
+            p.destacado == true
+        )
+    }
+
+    getCarrito() {
+        return this.cart;
+    }
+}
