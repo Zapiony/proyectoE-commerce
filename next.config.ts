c41bff63850565ba818ea444d18d46afa88f6df2
Zapiony <sazapatac4@gmail.com>
2026-01-12 17:03:04 -0500

    Configuración para ejecución correcta

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['local-origin.dev', '*.local-origin.dev'],
+    },
+  },
 };
 
 export default nextConfig;
